e619da32e6d93b9f9f365fc3d75f84f1cd87e797
Eduardo <xeduxbrux@gmail.com>
2021-08-20 08:12:08 -0300

    new: mexendo com banco de dados

---
+++ loja+virtual/src/br/com/alura/banco/TestaInsercao.java
@@ -0,0 +1,25 @@
+package br.com.alura.banco;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaInsercao {
+	public static void main(String[] args) throws SQLException  {
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection connection = connectionFactory.recuperaConexao();
+		
+		Statement stm = connection.createStatement();
+		stm.execute("INSERT INTO PRODUTO (nome, descricao) VALUES ('mouse' , 'mouse sem fio')", Statement.RETURN_GENERATED_KEYS);
+		
+		
+		ResultSet rst = stm.getGeneratedKeys();
+		
+		while(rst.next()) {
+			Integer id = rst.getInt(1); 
+			System.out.println("O valor do novo id é " +  id);
+		}
+		 
+	}
+}
